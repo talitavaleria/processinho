f5348e7917f78d9a986ad8b23685c3f6e54aea0d
Sarah Yasmini <sarahyasmini.ee@gmail.com>
2016-11-21 18:53:13 -0300

    rom

--- processinho.v
+++ processinho.v
@@ -10,8 +10,7 @@ module processinho(
 	input grab_ula,            // salva o resultado na ula no buffer
 	input[3:0] ula_operation,  // operaco a ser realizada na ula
 	inout[7:0] data_bus,  	      // dado a ser armazenado
-	//input pc_increment,
-	//input pc_cout,
+	input pc_increment,
 	output[7:0] result,
 	output[7:0] HEX0,
 	output[7:0] HEX1,
@@ -20,25 +19,25 @@ module processinho(
 );
 
 reg reset;
-wire pc_increment;
-wire pc_count;
-//wire[7:0] data_bus;
+reg [2:0]pc_count;
+wire[2:0] opcode;
 
 initial
 begin
 	reset = 1'b0;
 	//data_bus = 8'b0;
 //	pc_increment = 1'b0;
-//	pc_count = 1'b0;
-	//ram_enable = 1;
+//	pc_count = 3'b0;
+//	rom_enable = 1;
 end
 
 /* 
  *  TODO - fazer o controle da RAM
  */
-ram ram_memory(clock, ram_enable, we, addr, data_in, data_out);
+ram ram_memory(clock, ram_enable, we, addr_ram, data_in, data_out);
+rom rom_memory(clock, rom_enable, pc_count, opcode);
 
-datapath dp(	clock, 
+/*datapath dp(	clock, 
 					reset, 
 					gp_read, 
 					gp_write, 
@@ -49,11 +48,14 @@ datapath dp(	clock,
 					pc_increment,
 					pc_count, 
 					HEX3, HEX2, HEX1, HEX0
-				);
+				);*/
 
 //assign data_bus = operando;
-assign result = data_bus;
-assign pc_count = 1'b0;
-assign pc_increment = 1'b0;
+assign result = opcode;
+assign rom_enable = 1;
+
+always @(posedge pc_increment) begin
+	pc_count = pc_count+3'b1 > 6 ? 3'b0 : pc_count+3'b1;
+end
 
 endmodule
--- rom.v
+++ rom.v
@@ -1,24 +1,25 @@
 module rom (
-	input enable, 
-	input[2:0] addr,
-	output reg[2:0] data_out
+	input clock, //clock do processador
+	input enable, //habilita a rom
+	input[2:0] addr, //endereço 
+	output reg[2:0] data_out // instruçao
 );
 
-reg [3:0]rom[6:0];
+reg [3:0]rm[6:0];
 
 initial begin
-	rom[0] = 3'b101;
-	rom[1] = 3'b010;
-	rom[2] = 3'b111;
-	rom[3] = 3'b110;
-	rom[4] = 3'b101;
-	rom[5] = 3'b100;
-	rom[6] = 3'b011;
+	rm[0] = 3'b101;
+	rm[1] = 3'b010;
+	rm[2] = 3'b111;
+	rm[3] = 3'b110;
+	rm[4] = 3'b101;
+	rm[5] = 3'b100;
+	rm[6] = 3'b011;
 end
 
-always @(1) begin
-  if (enable == 1) begin
-		data_out <= rom[addr];
+always @(posedge clock) begin
+  if (enable == 1'b1) begin
+		data_out = rm[addr];
 	end
 end
 
